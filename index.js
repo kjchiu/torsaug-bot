60c6d0221ba0a424b4e7459e713eae1030de36bc
kjchiu <krypes@gmail.com>
2020-08-14 11:29:34 -0400

    account for jerks doing multiple queries in a message

--- index.js
+++ index.js
@@ -14,15 +14,18 @@ client.once('ready', () => {
 client.on('message', async msg => {
     if (msg.author.bot || ! formatter.isLoaded) { return; }
 
-    let [_, query] = /\[\[(.*)\]\]/.exec(msg.content) || [];
-    if (!query) { return; }
-    let card = cards.find(query);
-    if (!card) {
-        return;
+    let regex = /\[\[([^\[\]]*)\]\]/g
+    for(let matches = regex.exec(msg.content); !!matches; matches = regex.exec(msg.content)) {
+        let [_, query] = matches;
+        let card = cards.find(query);
+        if (!card) {
+            return;
+        }
+
+        let embed = formatter.format(card);
+        msg.channel.send(embed);
     }
 
-    let embed = formatter.format(card);
-    msg.channel.send(embed);
 });
 
 cards.load().then(() => client.login(secret));
